6d2eee47f14c4cb2a2ef35e3c9926544b4ef1924
Tifni Shroll <tasshroll@gmail.com>
2023-07-01 15:43:39 -0700

    Add functionality to GET from IndexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,6 +22,8 @@ export const putDb = async (content) => {
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
+
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
